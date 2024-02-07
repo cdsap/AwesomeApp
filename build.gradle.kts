7f752ca89de7fefce2e44207cd1f55f58d0f332b
Inaki Villar <inaki.seri@gmail.com>
2024-02-07 08:42:04 -0800

    adding tags

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+import io.github.cdsap.talaiot.metrics.Metrics
+
 plugins {
-    id("org.jetbrains.kotlin.jvm") version("1.9.10") apply false
+    id("org.jetbrains.kotlin.jvm") version ("1.9.10") apply false
     id("com.android.application") version "8.1.4" apply false
     id("com.android.library") version "8.1.4" apply false
     id("io.github.cdsap.talaiot") version "2.0.3"
@@ -15,9 +17,19 @@ talaiot {
             taskMetricName = "taskMetric"
             url = System.getenv("INFLUX_URL")
             buildTags = listOf(io.github.cdsap.talaiot.metrics.BuildMetrics.RequestedTasks)
-            taskTags = listOf(io.github.cdsap.talaiot.metrics.TaskMetrics.Task,
+            taskTags = listOf(
+                io.github.cdsap.talaiot.metrics.TaskMetrics.Task,
                 io.github.cdsap.talaiot.metrics.TaskMetrics.Module,
-                io.github.cdsap.talaiot.metrics.TaskMetrics.State)
+                io.github.cdsap.talaiot.metrics.TaskMetrics.State
+            )
         }
+    }
+    metrics {
+        customBuildMetrics(
+            "CI" to System.getenv("CI"),
+        )
+        customTaskMetrics(
+            "CI" to System.getenv("CI")
+        )
     }
 }
